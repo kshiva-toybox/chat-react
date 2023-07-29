0c129ea87bf790e90d116610957285b59489084b
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2023-07-29 23:14:47 +0900

    feat: コメント取得、投稿API繋ぎ込みまで

---
+++ src/hooks/Room/useGetComments.ts
@@ -0,0 +1,31 @@
+import useSWR from 'swr'
+import api from '../../utils/api'
+
+type Comment = {
+  id: number
+  stream: {
+    id: number
+    name: string
+  }
+  nickname: string
+  body: string
+}
+
+type Data = {
+  data: Comment[]
+}
+
+const fetcher = (url: string): Promise<Data> => api.get(url).then(res => res.data)
+
+const useGetComments = (id?: string) => {
+  const { data, error, mutate } = useSWR(id && `/api/streams/${id}/comments`, fetcher)
+
+  return {
+    comments: data,
+    isLoading: !error && !data,
+    isError: error,
+    mutate,
+  }
+}
+
+export default useGetComments
--- src/pages/ChatDetail.tsx
+++ src/pages/ChatDetail.tsx
@@ -1,22 +1,43 @@
 import { useParams } from "react-router-dom";
 import useGetDetail from "../hooks/Room/useGetDetail";
+import { FormEvent } from "react";
+import api from "../utils/api";
+import useGetComments from "../hooks/Room/useGetComments";
 
 const ChatDetail = () => {
   const { id } = useParams();
-  const { room, isLoading, isError } = useGetDetail(id);
+  const { room, isLoading: isRoomLoading, isError: isRoomError } = useGetDetail(id);
+  const { comments, mutate: commentsMutate, isLoading: isCommentsLoading, isError: isCommentsError } = useGetComments(id);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const nickname = formData.get('nickname');
+    const body = formData.get('body');
+
+    await api.post(`/streams/${id}/comments`, formData);
+  }
+
+  console.log({ comments })
 
   return (
     <div>
       <h1>ChatDetail</h1>
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Error</p>}
+      {isRoomLoading && <p>Loading...</p>}
+      {isRoomError && <p>Error</p>}
       {room && <h2>{room.data.name}</h2>}
 
       <h3>Messages</h3>
+      {isCommentsLoading && <p>Loading...</p>}
+      {isCommentsError && <p>Error</p>}
+      {comments && comments.data.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.nickname}: {comment.body}</p>
+        </div>
+      ))}
 
-
-
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input style={{ width: '100px' }} type="text" name="nickname" id="nickname" placeholder="nickname" />
         <input type="text" name="body" id="body" placeholder="body" />
         <button type="submit">Send</button>
